be487fabfa636b7558b8c77fada95a3fda055b81
Du7aa <105181425+Du7aa@users.noreply.github.com>
2022-06-03 19:03:43 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+#challenge1 in Programming Foundations : Fundamentals ,linked in learning
+#output of following code
+print("Challenge 1:")
+message="this is going to be tricky;)"
+Message="Very tricky"
+print(message)
+result=2**3
+print("2**3=",result)
+result=5-3
+print("5-3=",result)
+print("Challenge complete!")
+solution="""
+Challenge 1:
+this is going to be tricky;)
+2**3=8
+5-3=2
+Challenge complete!
+"""
+#challange 2
+fav_food=input("what's your fav food?")
+if fav_food=="kabsa":
+    print(fav_food,"yup!so amazing!")
+else:
+   print("Yucky!that's not it !")
+print("Thanks for playing")
+
+#Challange 3
+def fav_city(name):
+    print("My favourite city is ",name )
+fav_city("Alex")
+fav_city("Cairo")
+fav_city("Tanta")
